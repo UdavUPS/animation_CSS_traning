85168a8ee30af1b83e6466cda883e9a1859fe701
Udav-94 <ups-fti@yandex.ru>
2024-11-09 19:25:14 +0300

    Выполнено задание, добавлен скрипт appveyor

--- src/common.js
+++ src/common.js
@@ -1,4 +1,29 @@
 import './css/resetStyle.css';
 import './css/style.css';
 
+const but = document.querySelector('.widget__but');
+const text = document.querySelector('.widget__text');
+var style = window.getComputedStyle(text, null);
+
+console.log(style.animationName);
+
+but.addEventListener('click', () => {
+    /* text.style.display = 'none'; */
+    /* if (style.display == 'none') {
+        text.style.display = 'block';
+    } else {
+        text.style.display = 'none';
+    } */
+   if (style.animationName == 's') {
+        text.style.animationName = 'd';
+    } else {
+        text.style.animationName = 's';
+    }
+
+
+
+
+
+})
+
 
